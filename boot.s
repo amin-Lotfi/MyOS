9d5fccda20db9cbb69feaa4a206515ff886565cc
Amin Lotfi <73550606+amin-Lotfi@users.noreply.github.com>
2025-02-12 04:13:54 +0330

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,29 +1,21 @@
-# set magic number to 0x1BADB002 to identified by bootloader 
 .set MAGIC,    0x1BADB002
 
-# set flags to 0
 .set FLAGS,    0
 
-# set the checksum
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-# set multiboot enabled
 .section .multiboot
 
-# define type to long for each data defined as above
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
 
-# set the stack bottom 
 stackBottom:
 
-# define the maximum size of stack to 512 bytes
 .skip 1024
 
 
-# set the stack top which grows from higher to lower
 stackTop:
 
 .section .text
@@ -33,16 +25,13 @@ stackTop:
 
 _start:
 
-  # assign current stack pointer location to stackTop
 	mov $stackTop, %esp
 
-  # call the kernel main source
 	call kernel_entry
 
 	cli
 
 
-# put system in infinite loop
 hltLoop:
 
 	hlt
